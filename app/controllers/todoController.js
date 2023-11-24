84f34867b15d8dcd8608ec10d88d506c33ab7455
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-11-24 16:22:35 +0700

    feat: add CRUD API (post,put,delete) operations to todoController

--- app/controllers/todoController.js
+++ app/controllers/todoController.js
@@ -12,6 +12,51 @@ const getTodos = (req, res) => {
   });
 };
 
+const addTodo = (req, res) => {
+  const { title } = req.body;
+  todoModel.addTodo(title, (error, todo) => {
+    if (error) {
+      return res.status(500).json({
+        error:
+          "Failed to add a new task. Looks like even the to-do list is on strike!",
+      });
+    }
+    res.status(201).json(todo);
+  });
+};
+
+const updateTodo = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  todoModel.updateTodo(id, title, (error, todo) => {
+    if (error) {
+      return res.status(500).json({
+        error:
+          "Failed to update a task. It seems our to-do list hit a snag in the upgrade department",
+      });
+    }
+    res.status(200).json(todo);
+  });
+};
+
+const deleteTodo = (req, res) => {
+  const { id } = req.params;
+
+  todoModel.deleteTodo(id, (error) => {
+    if (error) {
+      return res.status(500).json({
+        error:
+          "Failed to delete a task. Looks like our to-do list is feeling a bit clingy!",
+      });
+    }
+    res.status(204).send();
+  });
+};
+
 module.exports = {
   getTodos,
+  addTodo,
+  updateTodo,
+  deleteTodo,
 };
