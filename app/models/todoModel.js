c18bb7cdc1e137cf2d54535ce695a3d5e23557c2
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-11-24 16:25:21 +0700

    feat: add CRUD API (post,put,delete) operations to todoModel

--- app/models/todoModel.js
+++ app/models/todoModel.js
@@ -9,6 +9,48 @@ const getAllTodos = (callback) => {
   });
 };
 
+const addTodo = (title, callback) => {
+  const query = {
+    text: "INSERT INTO todo (title) VALUES ($1) RETURNING *",
+    values: [title],
+  };
+
+  db.query(query, (error, results) => {
+    if (error) {
+      return callback(error, null);
+    }
+    callback(null, results.rows[0]);
+  });
+};
+
+const updateTodo = (id, title, callback) => {
+  const query = {
+    text: "UPDATE todo SET title = $1 WHERE id = $2 RETURNING *",
+    values: [title, id],
+  };
+
+  db.query(query, (error, results) => {
+    if (error) {
+      return callback(error, null);
+    }
+    callback(null, results.rows[0]);
+  });
+};
+
+const deleteTodo = (id, callback) => {
+  const query = {
+    text: "DELETE FROM todo WHERE id = $1",
+    values: [id],
+  };
+
+  db.query(query, (error) => {
+    if (error) {
+      return callback(error);
+    }
+    callback(null);
+  });
+};
+
 module.exports = {
   getAllTodos,
 };
